5c99004d816580ae6379db754f41ff843a023e34
harshithreddy6322 <harshithreddy6322@gmail.com>
2024-09-08 12:55:08 +0530

    api

--- backend_less/api/app.js
+++ backend_less/api/app.js
@@ -1,4 +1,5 @@
 // app.js
+
 import express, { json } from "express";
 import { createTransport } from "nodemailer";
 import dotenv from "dotenv";
@@ -6,19 +7,17 @@ dotenv.config();
 import { rateLimit } from "express-rate-limit";
 import zod from "zod";
 import cors from "cors";
+
 const app = express();
 
 app.use(json());
 app.use(cors());
 
-// app.use(cors());
-const PORT = 3000;
-
 const rateemail = rateLimit({
-  windowMs: 1 * 60 * 1000, // in 1min
-  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 1 * 60 * 1000, // in 1 min
+  limit: 10, // Limit each IP to 10 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 const transporter = createTransport({
@@ -35,33 +34,31 @@ const zodemail = zod.object({
   message: zod.string().min(3),
 });
 
-app.post("/send-email", rateemail, (req, res) => {
+app.post("/api/send-email", rateemail, (req, res) => {
   const body = req.body;
 
   const { success, error } = zodemail.safeParse(body);
-  if (!success)
+  if (!success) {
     return res.status(400).json({
       status: 400,
-      error: "worng inputs",
+      error: "Invalid input",
       message: error.issues[0].message,
     });
+  }
 
   const mailOptions = {
-    from: process.env.EMAIL, // Sender address (can also be your email)
+    from: process.env.EMAIL,
     to: process.env.EMAIL, // Your email address to receive the email
-    subject: `Message from ${body.fullname}`, // Subject line
-    text: `Message from ${body.fullname} (${body.email}): ${body.message}`, // Plain text body
+    subject: `Message from ${body.fullname}`,
+    text: `Message from ${body.fullname} (${body.email}): ${body.message}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error); // Log the error
       return res.status(500).json({ message: "Error sending email", error });
     }
     res.status(200).json({ message: "Email sent successfully" });
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,6 +7,7 @@ function Card({ name, imge, src }) {
           src={imge}
           alt={name}
           className="w-[100%] h-[165px] object-cover object-center bg-white rounded-t-lg max-lg:w-[100%] max-lg:h-[100%]"
+          loading="lazy"
         />
         <div className="p-4 bg-white text-black rounded-b-lg">
           <h1 className=" capitalize font-medium text-lg text-center">
