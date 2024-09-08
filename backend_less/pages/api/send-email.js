c4dd25d585e882ccd5a63e32e3451ada099f6266
harshithreddy6322 <harshithreddy6322@gmail.com>
2024-09-08 13:03:57 +0530

    api

--- backend_less/pages/api/send-email.js
+++ backend_less/pages/api/send-email.js
@@ -1,21 +1,19 @@
-// app.js
-
-import express, { json } from "express";
 import { createTransport } from "nodemailer";
 import dotenv from "dotenv";
-dotenv.config();
 import { rateLimit } from "express-rate-limit";
 import zod from "zod";
-import cors from "cors";
+import express from "express";
+import { createServer } from "vercel";
 
+dotenv.config();
 const app = express();
 
-app.use(json());
-app.use(cors());
+app.use(express.json());
 
+// Rate limiting setup
 const rateemail = rateLimit({
-  windowMs: 1 * 60 * 1000, // in 1 min
-  limit: 10, // Limit each IP to 10 requests per windowMs
+  windowMs: 1 * 60 * 1000,
+  limit: 10,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -23,8 +21,8 @@ const rateemail = rateLimit({
 const transporter = createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL, // Your email address
-    pass: process.env.PASSWORD, // Your email password (App Password recommended)
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
 
@@ -36,19 +34,18 @@ const zodemail = zod.object({
 
 app.post("/api/send-email", rateemail, (req, res) => {
   const body = req.body;
-
   const { success, error } = zodemail.safeParse(body);
   if (!success) {
     return res.status(400).json({
       status: 400,
-      error: "Invalid input",
+      error: "wrong inputs",
       message: error.issues[0].message,
     });
   }
 
   const mailOptions = {
     from: process.env.EMAIL,
-    to: process.env.EMAIL, // Your email address to receive the email
+    to: process.env.EMAIL,
     subject: `Message from ${body.fullname}`,
     text: `Message from ${body.fullname} (${body.email}): ${body.message}`,
   };
@@ -61,4 +58,4 @@ app.post("/api/send-email", rateemail, (req, res) => {
   });
 });
 
-export default app;
+export default createServer(app);
